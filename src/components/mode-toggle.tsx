b161295412a7cfbf36101b06b08f3be09ad58982
Alireza Nikzad <nikzad.alireza79@gmail.com>
2026-05-26 01:50:07 +0330

    feat: setup dark mode

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,21 @@
+import { Moon, Sun } from 'lucide-react'
+
+import { useTheme } from '@/components/theme-provider'
+import { Button } from '@/components/ui/button'
+
+export function ModeToggle() {
+  const { setTheme } = useTheme()
+
+  const toggleTheme = () => {
+    const isDark = document.documentElement.classList.contains('dark')
+    setTheme(isDark ? 'light' : 'dark')
+  }
+
+  return (
+    <Button variant="outline" size="icon" onClick={toggleTheme}>
+      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  )
+}
