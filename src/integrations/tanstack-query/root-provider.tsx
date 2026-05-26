468f3bd9d5d5fac7cb697c277fd9b25f58bf93b2
Alireza Nikzad <nikzad.alireza79@gmail.com>
2026-05-26 11:09:22 +0200

    chore: update package.json and pnpm workspace configuration; enhance query client options
    - Remove unused pnpm configuration from package.json.
    - Add shellEmulator and trustPolicy settings to pnpm workspace configuration.
    - Update the QueryClient instantiation in root-provider.tsx to include default options for query behavior.

--- src/integrations/tanstack-query/root-provider.tsx
+++ src/integrations/tanstack-query/root-provider.tsx
@@ -1,7 +1,18 @@
 import { QueryClient } from '@tanstack/react-query'
 
 export function getContext() {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        // staleTime: 0,
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        refetchOnReconnect: false,
+        refetchInterval: false,
+        refetchIntervalInBackground: false,
+      },
+    },
+  })
 
   return {
     queryClient,
